17a977c4bd6cb51be18589b72daf81394fca621b
Yun Ei Hlaing <50979700+yuneihlaing@users.noreply.github.com>
2026-03-18 19:54:27 -0700

    modified next.config

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -2,12 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "img.youtube.com" },
       { protocol: "https", hostname: "i.ytimg.com" },
       { protocol: 'https', hostname: 'escholarship.org' },
     ],
   },
+  output: "export",
+  basePath: "/my-portfolio",
+  assetPrefix: "/my-portfolio/"
 };
 
 export default nextConfig;
